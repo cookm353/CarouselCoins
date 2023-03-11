52015d7d9f6dfc262323e12937894b0c9017e06d
Matthew Cook <cookm353@gmail.com>
2023-03-11 15:47:21 -0500

    Implemented all tests

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,18 +2,21 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
-// Smoke test
+/* Smoke test */
+
 it("renders", () => {
   render(<Carousel />)
 })
 
-// Snapshot test
+/* Snapshot test */
+
 it("matches snapshot", () => {
   const { asFragment } = render(<Carousel />)
   expect(asFragment()).toMatchSnapshot()
 })
 
-// Tests for right arrow
+/* Tests for right arrow */
+
 it("works when you click on the right arrow", function() {
   const { queryByTestId, queryByAltText } = render(<Carousel />);
 
@@ -30,7 +33,24 @@ it("works when you click on the right arrow", function() {
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
 
-// tests for left arrow
+it("doesn't show right arrow on last image", () => {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+  const rightArrow = queryByTestId("right-arrow")
+
+  // verify the right arrow is in the document
+  expect(rightArrow).toBeInTheDocument()
+
+  // move to last pic
+  fireEvent.click(rightArrow)
+  fireEvent.click(rightArrow)
+  
+  // verify it's on the last pic
+  expect(queryByAltText("Photo by Josh Post on Unsplash")).toBeInTheDocument()
+  expect(rightArrow).not.toBeInTheDocument()
+})
+
+/* tests for left arrow */
+
 it("works when you click on the left arrow", () => {
   const { queryByTestId, queryByAltText } = render(<Carousel />)
   
@@ -49,4 +69,13 @@ it("works when you click on the left arrow", () => {
   // expect first image to show, not the second
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+})
+
+it("doesn't show the left arrow on the first pic", () => {
+  const { queryByTestId, queryByAltText } = render(<Carousel />)
+  const leftArrow = queryByTestId("left-arrow")
+
+  // verify the first pic is showing
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(leftArrow).not.toBeInTheDocument()
 })
